91b7c5d0308fe9fc5f0e9813ed7733286c4df2d6
Evan Johnston <evanjohnman@gmail.com>
2021-09-29 17:27:25 -0500

    fix incorrect reference for NoResponseError in a2s.py

--- utils/servers/a2s.py
+++ utils/servers/a2s.py
@@ -1,6 +1,7 @@
 from hikari.errors import ForbiddenError
 import a2s
 import valve.rcon
+import valve.source.a2s
 from utils.servers.base import BaseServer
 import asyncio
 
@@ -26,7 +27,7 @@ class A2SCompatibleServer(BaseServer):
                                                       f"Mem: {round(self.proc.memory_percent(), 2)}%")
             except ForbiddenError:
                 print("Bot lacks permission to edit channels. (hikari.ForbiddenError)")
-            except valve.source.NoResponseError:
+            except valve.source.a2s.NoResponseError:
                 print("No Response from server before timeout (NoResponseError)")
             except Exception as e:
                 print(f"Error: {e} {type(e)}")
