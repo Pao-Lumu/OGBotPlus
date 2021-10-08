942a03b165005eff0cca8cc751d478d99637d08e
Evan Johnston <evanjohnman@gmail.com>
2021-10-07 23:41:49 -0500

    Testing

--- utils/sensor.py
+++ utils/sensor.py
@@ -26,7 +26,8 @@ def get_running() -> List[Tuple[str, psutil.Process]]:
         elif psutil.LINUX:
             ps: psutil.Popen = psutil.Popen(r"/usr/sbin/ss -tulpn | grep -P :2222\d*", shell=True,
                                             stdout=PIPE, stderr=DEVNULL)
-            pids = re.findall(r'(2222\d)(?<=pid=)(\d+)', ps.stdout.read().decode("utf-8"))
+            raw = ps.stdout.read().decode("utf-8")
+            pids = re.findall(r'(2222\d)(?<=pid=)(\d+)', raw)
             print(pids)
             procs = [(port, proc) for port, proc in [(port, psutil.Process(pid=x)) for port, x in pids] if
                      proc.username() == psutil.Process().username()]
