46d5bfb22841b093fae7b259fb39e09ae17369a8
Evan Johnston <evanjohnman@gmail.com>
2021-11-05 19:28:11 -0500

    okay different exception this time

--- utils/servers/docker_minecraft.py
+++ utils/servers/docker_minecraft.py
@@ -90,8 +90,6 @@ class MinecraftDockerServer(BaseDockerServer):
             for chan in self.bot.chat_channels_obj:
                 await chan.send(x, user_mentions=mentioned_users)
         for msg in msgs:
-            print(type(msg))
-            print(msg)
             self.bot.bprint(f"{self._repr} | {''.join(msg[0])}")
 
     def remove_nestings(self, iterable):
