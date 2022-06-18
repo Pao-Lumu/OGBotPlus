f996a83ee999b0e87e8094a277624aaf2f645880
Evan Johnston <evanjohnman@gmail.com>
2022-06-18 01:25:55 -0500

    patch warframe.py (because I haven't set up Robomaya yet)

--- plugins/warframe.py
+++ plugins/warframe.py
@@ -1,7 +1,5 @@
 import asyncio
 import json
-# import pprint
-import pprint
 import random
 import re
 import time
@@ -11,64 +9,50 @@ import aiohttp
 import hikari
 import lightbulb
 import pytz
+from lightbulb import commands
 
-plugin = lightbulb.Plugin("Warframe")
+plugin = lightbulb.Plugin(name="Warframe", description="Various commands relating to Warframe and the Warframe Market")
 
 try:
     with open('data/wf_market_items.json') as file:
         x = json.load(file)
-        wf_mark_items = x['items']
-        wf_mark_last_update = x['last_update']
+        wf_mkt_items = x['items']
+        wf_mkt_last_update = x['last_update']
 except (FileNotFoundError, json.JSONDecodeError):
-    wf_mark_items = []
-    wf_mark_last_update = 0
+    wf_mkt_items = []
+    wf_mkt_last_update = 0
 
 
-async def fetch(session, url):
+async def get(session: aiohttp.ClientSession, url: str):
     async with session.get(url) as response:
-        if response.status == 404:
-            raise NameError('That is not a valid item. Please check your spelling.')
+        if response.status != 200:
+            raise NameError(f'Request threw error {response.status}')
         else:
             return await response.text()
 
 
-async def get_items():
+async def get_item_info(name=None):
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, f"https://api.warframe.market/v1/items")
-        j = json.loads(html)['payload']['items']
-        return j
-
-
-async def get_item_info(name):
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, f"https://api.warframe.market/v1/items/{name}")
-        j = json.loads(html)
-        return j
+        response = await get(session, f"https://api.warframe.market/v1/items{'/' + name if name else ''}")
+        return json.loads(response) if name else json.loads(response)['payload']['items']
 
 
 async def get_item_orders(name):
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, f"https://api.warframe.market/v1/items/{name}/orders")
-        j = json.loads(html)
-        return j
+        response = await get(session, f"https://api.warframe.market/v1/items/{name}/orders")
+        return json.loads(response)
 
 
 async def get_item_statistics(name):
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, f"https://api.warframe.market/v1/items/{name}/statistics?include=item")
-        j = json.loads(html)
-        return j
+        response = await get(session, f"https://api.warframe.market/v1/items/{name}/statistics?include=item")
+        return json.loads(response)
 
 
 @plugin.command
-@lightbulb.add_checks(lightbulb.human_only)
-@lightbulb.command("baro",
-                   """Tells you where and when Baro Ki'Teer is coming, and what he's selling when he is here.""",
-                   aliases=['when_baro'])
-@lightbulb.implements(lightbulb.commands.PrefixCommand)
-async def baro(ctx):
-    """Tells you where and when Baro Ki'Teer is coming, and what he's selling when he is here."""
-
+@lightbulb.command("baro", "See when and where Baro Ki'teer is coming, and what he has when he is")
+@lightbulb.implements(commands.SlashCommand, lightbulb.commands.PrefixCommand)
+async def baro(ctx: lightbulb.context.Context) -> None:
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.warframestat.us/pc/voidTrader') as resp:
             info = await resp.json()
@@ -80,38 +64,36 @@ async def baro(ctx):
 
     c = hikari.Colour.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     if not info['active']:
-        e = hikari.Embed(title=f'{info["character"]} (Void Trader)', color=c,
-                         # description="{} will arrive at {} on {} ({}) and will stay until {} ({})".format(
-                         #     info['character'], info['location'], hr_active, info['startString'], hr_expiry,
-                         #     info['endString'])
-                         )
-        e.add_field("Location", info['location'])
-        e.add_field("Arrival Date", hr_active, inline=True)
-        e.add_field("Time until Arrival", info['startString'], inline=True)
-        e.add_field("-=-=-=-=-=-=-=-=-", "-=-=-=-=-=-=-=-=-")
-        e.add_field("Departure Date", hr_expiry, inline=True)
-        e.add_field("Time until Departure", info['endString'], inline=True)
-        await ctx.respond(embed=e)
+        embd = hikari.Embed(title=f'{info["character"]} (Void Trader)', color=c,
+                            # description="{} will arrive at {} on {} ({}) and will stay until {} ({})".format(
+                            #     info['character'], info['location'], hr_active, info['startString'], hr_expiry,
+                            #     info['endString'])
+                            )
+        embd.add_field("Location", info['location']) \
+            .add_field("Arrival Date", hr_active, inline=True) \
+            .add_field("Time until Arrival", info['startString'], inline=True) \
+            .add_field("-=-=-=-=-=-=-=-=-", "-=-=-=-=-=-=-=-=-") \
+            .add_field("Departure Date", hr_expiry, inline=True) \
+            .add_field("Time until Departure", info['endString'], inline=True)
+        await ctx.respond(embed=embd)
     else:
-        e = hikari.Embed(title="Void Trader Offerings", color=c)
-        e.set_footer(text=info["character"])
+        embd = hikari.Embed(title="Void Trader Offerings", color=c)
+        embd.set_footer(text=info["character"])
         for offer in info['inventory']:
-            e.add_field(name=offer['item'], value=f"{offer['ducats']} ducats + {offer['credits']} credits",
-                        inline=True)
+            embd.add_field(name=offer['item'], value=f"{offer['ducats']} ducats + {offer['credits']} credits",
+                           inline=True)
 
-        dukey = "{0} is currently at {1}, and will leave on {2} {3}.".format(info['character'], info['location'],
-                                                                             hr_expiry, info['endString'])
+        message = "{0} is currently at {1}, and will leave on {2} {3}.".format(info['character'], info['location'],
+                                                                               hr_expiry, info['endString'])
 
-        await ctx.respond(dukey, embed=e)
+        await ctx.respond(message, embed=embd)
 
 
 @plugin.command
 @lightbulb.add_checks(lightbulb.human_only)
 @lightbulb.command("nightwave", """Lists all current Nightwave missions""", aliases=['nw'])
-@lightbulb.implements(lightbulb.commands.PrefixCommand)
+@lightbulb.implements(commands.SlashCommand, commands.PrefixCommand)
 async def nightwave(ctx):
-    """Lists all current Nightwave missions"""
-
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.warframestat.us/pc/nightwave') as resp:
             info = await resp.json()
@@ -142,9 +124,8 @@ async def nightwave(ctx):
 @plugin.command
 @lightbulb.add_checks(lightbulb.human_only)
 @lightbulb.command("rifts", """Lists all current void fissures""", aliases=['voidrifts', 'rift', 'void'])
-@lightbulb.implements(lightbulb.commands.PrefixCommand)
+@lightbulb.implements(commands.SlashCommand, commands.PrefixCommand)
 async def rifts(ctx):
-
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.warframestat.us/pc/fissures') as resp:
             info = await resp.json()
@@ -158,12 +139,12 @@ async def rifts(ctx):
                     last_rift['tierNum'] != rift['tierNum']):
                 e.add_field(
                     name='-=-=-=-=-=-=-=-=-',
-                    value=f"***{rift['tier']} {'Rifts' if not rift['isStorm'] else 'Void Storms'}***",
+                    value=f"__***{rift['tier']} {'Rifts' if not rift['isStorm'] else 'Void Storms'}***__",
                     inline=False)
                 last_rift = rift
 
             if rift.setdefault('expired', None):
-                print(rift)
+                # print(rift)
                 continue
             node = re.sub(r"\)", r" Proxima)", rift['node']) if rift['isStorm'] else rift['node']
             e.add_field(
@@ -173,28 +154,28 @@ async def rifts(ctx):
         await ctx.respond(embed=e)
     else:
         await ctx.respond(
-            "No void storms are active. If you can read this, the API this bot uses is probably bugged out.")
+            "No void fissures are active. If you can read this, the API this bot uses is probably bugged out.")
         return
 
 
 @plugin.command
 @lightbulb.add_checks(lightbulb.human_only)
+@lightbulb.option("item", "Item to search the market for (type at least 3 chars for autocompletion)", required=True, autocomplete=True)
 @lightbulb.command("pricecheck", """Check prices of items on warframe.market""", aliases=["pc", "PC", "Pc", "pC"])
-@lightbulb.implements(lightbulb.commands.PrefixCommand)
+@lightbulb.implements(commands.SlashCommand, commands.PrefixCommand)
 async def pricecheck(ctx):
-    global wf_mark_items
-    global wf_mark_last_update
-    item = ctx.options.text
-    name = str(item).lower()
+    global wf_mkt_items
+    global wf_mkt_last_update
+    name = str(ctx.options.item).lower()
     if time.mktime(
-            datetime.utcnow().timetuple()) > wf_mark_last_update + 60 * 60 * 60 or not wf_mark_items:
-        wf_mark_items = await get_items()
-        # pprint.pprint(wf_mark_items)
-        wf_mark_last_update = time.mktime(datetime.utcnow().timetuple())
+            datetime.utcnow().timetuple()) > wf_mkt_last_update + 60 * 60 * 60 or not wf_mkt_items:
+        wf_mkt_items = await get_item_info()
+        wf_mkt_last_update = time.mktime(datetime.utcnow().timetuple())
         with open('data/wf_market_items.json', 'w') as file:
-            json.dump({"items": wf_mark_items, "last_update": wf_mark_last_update}, file)
+            json.dump({"items": wf_mkt_items, "last_update": wf_mkt_last_update}, file)
     fetchable_items = []
-    for item in wf_mark_items:
+    for item in wf_mkt_items:
+        # print(item)
         if item.get('url_name'):
             if name in item.get('item_name').lower() or name in item.get('url_name'):
                 fetchable_items.append((item['item_name'], item['url_name']))
@@ -210,9 +191,8 @@ Search matched too many items.
 Please be more specific.
 (Found {len(fetchable_items)} items matching search)""")
     elif len(fetchable_items) < 1:
-        await ctx.respond(f"""No item with that name found. Check spelling?""")
+        await ctx.respond(f"""No item with name {name} found. Check spelling?""")
     else:
-        pprint.pprint(fetchable_items)
         for item_name, name in fetchable_items:
             try:
                 item_stats, item_orders, item_info = await asyncio.gather(
@@ -250,13 +230,13 @@ Please be more specific.
                     td_vol.append(stat['volume'])
                     td_avg.append(stat['avg_price'])
             vol = sum(td_vol)
-            avg = sum(td_avg) / len(td_avg)
+            avg = sum(td_avg) / (len(td_avg) | 1)
 
             e = hikari.Embed()
-            e.title = item['en']['item_name']
-            e.url = 'https://warframe.market/items/' + item['url_name']
+            e.title = item_info['en']['item_name']
+            e.url = 'https://warframe.market/items/' + item_info['url_name']
             e.set_author(name='Warframe.market price check')
-            e.set_image('https://warframe.market/static/assets/' + item['icon'])
+            e.set_image('https://warframe.market/static/assets/' + item_info['icon'])
             e.set_footer(text='warframe.market')
             e.add_field("Average price (48hrs)", f"{round(avg)}p", inline=True)
             e.add_field("Trade Volume (48hrs)", f"{vol} items traded in the last 48hrs", inline=True)
@@ -296,3 +276,29 @@ async def steelpath(ctx):
     for item in info['evergreens']:
         e.add_field(name=f"{item['name']}", value=f"({item['cost']} Steel Essence)", inline=True)
     await ctx.respond(embed=e)
+
+
+@pricecheck.autocomplete('item')
+async def pc_autocomp(opt: hikari.AutocompleteInteractionOption, autocomp):
+    if len(opt.value) < 3:
+        return 'Please type at least 3 characters'
+    if wf_mkt_items:
+        print(opt.value)
+        print([item.get('item_name') for item in wf_mkt_items])
+        response = [item.get('item_name') for item in wf_mkt_items if opt.value.lower() in item.get('item_name').lower()]
+        print(response)
+        response.sort()
+        if len(response) > 25:
+            return response[:25]
+        else:
+            return response
+    else:
+        return 'Item Not Found'
+
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+
+def unload(bot):
+    bot.remove_plugin(plugin)
